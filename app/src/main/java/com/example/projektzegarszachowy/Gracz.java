62e58aa1362f2f7fc452ea8e95a1108d48a9f05d
BartekK022 <baklu100508@gmail.com>
2026-03-06 14:12:15 +0100

    zrobiona klasa

---
+++ app/src/main/java/com/example/projektzegarszachowy/Gracz.java
@@ -0,0 +1,52 @@
+package com.example.projektzegarszachowy;
+
+import android.os.CountDownTimer;
+import android.widget.Button;
+
+import java.nio.Buffer;
+
+public class Gracz {
+    private int ileSekund;
+    private CountDownTimer countDownTimer;
+    private Button button;
+    private boolean czyAktywny;
+
+    public Gracz(Button button, boolean czyAktywny) {
+        this.button = button;
+        this.czyAktywny = czyAktywny;
+        ileSekund = 180; // nasz zegar na razie na 3 minuty
+        if(czyAktywny) {
+            uruchomZegar();
+        } else {
+            StopZegar();
+        }
+    }
+
+    public void odwrocAktywnosc() {
+        czyAktywny =! czyAktywny;
+        if (czyAktywny) {
+            uruchomZegar();
+        } else {
+            StopZegar();
+        }
+    }
+
+    private void uruchomZegar(){
+        countDownTimer = new CountDownTimer(ileSekund, 1000) {
+            @Override
+            public void onFinish() {
+
+            }
+
+            @Override
+            public void onTick(long l) {
+                ileSekund = (int) (l/1000);
+                button.setText(""+ileSekund);
+            }
+        };
+        countDownTimer.start();
+    }
+    private void StopZegar() {
+        countDownTimer.cancel();
+    }
+}
